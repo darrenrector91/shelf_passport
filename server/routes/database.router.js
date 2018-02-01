6792f4c3381a098036a2189970c6d13ab5b63571
Darian N <darian.nasrabadi@gmail.com>
2018-02-01 15:47:50 -0600

    changed get call name to be fixed

--- server/routes/database.router.js
+++ server/routes/database.router.js
@@ -10,7 +10,7 @@ const router = express.Router();
 // let Item = mongoose.model('Item', itemSchema);
 
 router.get('/', (req, res) => {
-    Item.find({}, (error, data) => {
+    schema.item.find({}, (error, data) => {
         if (error) {
             console.log('error on finding items', error);
             res.sendStatus(500)
