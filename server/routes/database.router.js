157e79bae0e95cc2762848284eb72b14d1d65455
Alaethia <truthcharis@gmail.com>
2018-02-02 09:37:54 -0600

    changed get

--- server/routes/database.router.js
+++ server/routes/database.router.js
@@ -10,7 +10,7 @@ const router = express.Router();
 // let Item = mongoose.model('Item', itemSchema);
 
 router.get('/', (req, res) => {
-    schema.item.find({}, (error, data) => {
+    schema.person.find({}, (error, data) => {
         if (error) {
             console.log('error on finding items', error);
             res.sendStatus(500)
